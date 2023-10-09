eb5658842e27fbee256760276e53a332d2a85a25
Tahsin0909 <tajisan789@gmail.com>
2023-10-09 12:21:04 +0600

    Updated Servisec Details and Working on album

--- src/Components/ServicesDetails/Packages.jsx
+++ src/Components/ServicesDetails/Packages.jsx
@@ -4,11 +4,12 @@ import ServiceByPackage from "./serviceByPackage";
 
 const Packages = ({ PackageData }) => {
     // console.log(PackageData)
-    const { packageName, description, price, includedServices } = PackageData
+    const { packageName, description, price, includedServices, id } = PackageData
 
     return (
         <div>
-            <div className="relative flex  max-w-[25rem] lg:h-[580px] flex-col rounded-xl bg-gradient-to-tr from-blue-900 to-zinc-600 bg-clip-border p-8 text-white shadow-md shadow-pink-500/40">
+            <div
+                className={`relative flex  max-w-[25rem] lg:h-[580px] flex-col rounded-xl bg-gradient-to-tr ${id == 1 ? "from-[#aeaeae] to-[#767272]" : id == 2 ? "from-[#ffe348] to-[#FFA500]" : id == 3 ? "from-blue-900 to-zinc-900" : `bg-red-400`}  bg-clip-border p-8 text-white shadow-md shadow-pink-500/40`}>
                 <div className="relative m-0 mb-8 overflow-hidden rounded-none border-b border-white/10 bg-transparent bg-clip-border pb-8 text-center  shadow-none">
                     <p className="block font-sans text-xl md:text-2xl  font-semibold uppercase leading-normal text-white antialiased ">
                         {packageName}
@@ -27,10 +28,10 @@ const Packages = ({ PackageData }) => {
                     }
                 </div>
                 <div className="mt-12 p-0">
-                    <button className="btn btn-primary w-full">Book Now</button>
+                    <button className={`btn  w-full ${id == 1 ? "bg-[#333] text-white" : id == 2 ? "bg-[#e3b241] text-white" : id == 3 ? "bg-white" : `bg-red-400`}`}>Book Now</button>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
